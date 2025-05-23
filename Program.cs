c1c0d6ff511aef83c972210553e344d4f4bc976a
betaRei <malthereippuert42@gmail.com>
2025-05-23 12:13:00 +0200

    added mutex

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿using var game = ZooTycoonManager.GameWorld.Instance;
-game.Run();
+﻿using System;
+using System.Threading;
+
+
+namespace ZooTycoonManager
+{
+    public static class Program
+    {
+        public static Mutex mutex;
+
+        [STAThread]
+        static void Main()
+        {
+            bool createdNew;
+            mutex = new Mutex(true, "GameMutex", out createdNew);
+
+            if (!createdNew)
+            {
+                Console.WriteLine("Already running");
+                return;
+            }
+
+            GameWorld.Instance.Run();
+            
+        }
+    }
+}
